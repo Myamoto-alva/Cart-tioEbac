57d3b26c607e9c876a9eb5f4dfd26e9dff1f9b1f
Myamoto-alva <163326539+Myamoto-alva@users.noreply.github.com>
2024-03-13 11:52:00 -0400

    Opção de saida
    Incluso no sistema a opção de saída para o usuário.

--- CatoriodaEBAC.c
+++ CatoriodaEBAC.c
@@ -130,6 +130,7 @@ int main()
 		printf("\t1 - Registrar nomes:\n\n");
 		printf("\t2 - Consultar nomes:\n\n");
 		printf("\t3 - Deleatr nomes\n\n");
+		printf("\t4 - Sair do sistema:\n\n");
 		printf("Opção: ");
 		
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -148,6 +149,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por ultilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
